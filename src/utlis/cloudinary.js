2fe2cc32e3af85afa9768e72d61351797b178c14
Nabil Ahmad Rifat <106903272+Nabil-Ahamad-Rifat@users.noreply.github.com>
2026-01-21 03:08:06 +0600

    multer and cludeinary setpu

---
+++ src/utlis/cloudinary.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from "fs"
+
+
+
+
+cloudinary.config({ 
+  cloud_name:process.env.CLUDEINARY_CLUDE_NAME, 
+  api_key: process.env.CLUDEINARY_CLUDE_API_KEY, 
+  api_secret: process.env.CLUDEINARY_CLUDE_API_SECRET,
+});
+
+const uploadOnCludeinary = async (localfilepath)=>{
+    try {
+        if(!localfilepath)return null;
+        const responce = await cloudinary.uploader.upload(localfilepath,{
+            resource_type:"auto",
+        })
+        //file has been upload successfully 
+        console.log("file uploaded on cloudeinary successfully",responce.url);
+        console.log(responce);
+        return responce;
+    } catch (error) {
+        fs.unlinkSync(localfilepath);
+        console.log("cloudinary upload error :",error);
+        return null;
+        
+    }
+}
+
+export {uploadOnCludeinary};
+// export {uploadOnCludeinary};
+
+
+
